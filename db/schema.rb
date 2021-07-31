dc7d787d1648145f1fe2980109c66d253246f278
M-Alamgir <ourandoc@gmail.com>
2021-07-31 23:57:05 +0500

    generate event scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_183413) do
+ActiveRecord::Schema.define(version: 2021_07_31_185552) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "time"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
