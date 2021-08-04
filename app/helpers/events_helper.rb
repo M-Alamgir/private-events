f703834978cb43c63656edcf56dc8462c4cf923b
M-Alamgir <ourandoc@gmail.com>
2021-08-04 16:01:20 +0500

    fix linters

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,12 @@
 module EventsHelper
   def attend_event
     if @event.attendees.exists?(current_user.id)
-      'You are Attending'
+      '<li class="bg-green-400 list-none py-2 text-center text-white 
+      font-bold font-mono rounded">You are Attending</li>'.html_safe
     else
-      link_to 'Attend this event', "/events/attend/#{@event.id}", method: :put,
-                                                                  class: 'bg-red-500 list-none py-3 px-16 text-center text-white font-bold font-mono rounded'
+      link_to 'Attend this Event', "/events/attend/#{@event.id}", method: :put,
+      class: 'bg-red-500 w-50 list-none py-3 px-16 text-center
+       text-white font-bold font-mono rounded'
     end
   end
 
